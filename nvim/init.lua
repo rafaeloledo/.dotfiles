a63f3342aedce923f44aecb34ff56e4847343bb4
rafaeloledo <rafaeloliveiraledo@gmail.com>
2024-12-01 03:09:59 -0300

    chore: update config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,12 +1,2 @@
--- vim.g.loaded_netrw = 1
--- vim.g.loaded_netrwPlugin = 1
-vim.opt.termguicolors = true
-
+-- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-require("plugins")
-require("keymaps")
-require("options")
-
-vim.g.netrw_browse_split = 0
-vim.g.netrw_banner = 0
-vim.g.netrw_winsize = 25
